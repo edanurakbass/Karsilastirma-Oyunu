acf9b8a47772edc0a46c2ac914c2f15658839f56
edanurakbass <edanurakbas6@gmail.com>
2020-10-18 18:11:20 +0300

    master

---
+++ Assets/Scripts/GameLevel/TimerManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text sureText;
+
+    int kalanSure;
+    bool sureSaysinMi = true;
+
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = Object.FindObjectOfType<GameManager>();
+    }
+    void Start()
+    {
+        kalanSure = 50;
+        sureSaysinMi = true;
+        
+    }
+
+    public void SureyiBaslat()
+    {
+        StartCoroutine(SureTimerRoutine());
+    }
+
+    IEnumerator SureTimerRoutine()
+    {
+        while (sureSaysinMi)
+        {
+            yield return new WaitForSeconds(2.5f);
+            if (kalanSure < 10)
+            {
+                sureText.text = "0" + kalanSure.ToString();
+            }
+            else
+            {
+                yield return new WaitForSeconds(1f);
+                sureText.text = kalanSure.ToString();
+                //kalanSure--;
+            }
+
+            if (kalanSure <= 0)
+            {
+                sureSaysinMi = false;
+                sureText.text = "";
+                gameManager.OyunuBitir();
+
+
+            }
+
+            kalanSure--;
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MenuLevel/MenuManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Transform kafa;
+
+    [SerializeField]
+    private Transform startBtn;
+    void Start()
+    {
+        kafa.transform.GetComponent<RectTransform>().DOLocalMoveX(0f, 1f).SetEase(Ease.OutBack); 
+        startBtn.transform.GetComponent<RectTransform>().DOLocalMoveY(-270f,1f).SetEase(Ease.InOutBack);
+    }
+
+    public void OyunaBasla()
+    {
+        SceneManager.LoadScene("GameLevel");
+    }
+   
+    void Update()
+    {
+        
+    }
+}
